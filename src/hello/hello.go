80d59d38bd4a76c5238184f6d2ffc1689a50a313
Nhat Bui <minhnhat992@gmail.com>
2021-08-30 11:28:44 -0400

    add new package

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"math"
+	"undefined/src/github.com/einsteinis/go_tutorial/string_util"
+
+
+)
 
 func main() {
-	fmt.Printf("hello world")
+	// bytes_tutorial()
+	// use_math_pck()
+	fmt.Println(string_util.Reverse("reverse me"))
+}
+
+// func main() {
+// 	my_byte := byte('b')
+// 	my_rune := '~'
+// 	fmt.Printf("%c=%d %c=%U\n", my_byte, my_rune, my_rune, my_rune)
+// }
+
+func bytes_tutorial() {
+	b1 := byte('a')
+	b2 := []byte("A")
+	b3 := []byte{'a', 'b', 'c'}
+	fmt.Printf("b1 = %c\n", b1)
+	fmt.Printf("b2 = %c\n", b2)
+	fmt.Printf("b3 = %s\n", b3)
+	s1 := []byte("Hello")
+	s2 := []byte("World")
+	s3 := [][]byte{s1, s2}
+	s4 := bytes.Join(s3, []byte(","))
+	s5 := []byte{}
+	s5 = bytes.Join(s3, []byte("--"))
+	s6 := [][]byte{[]byte("foo"), []byte("bar"), []byte("baz")}
+	fmt.Printf("s1 = %s\n", s1)
+	fmt.Printf("s2 = %s\n", s2)
+	fmt.Printf("s3 = %s\n", s3)
+	fmt.Printf("s4 = %s\n", s4)
+	fmt.Printf("s5 = %s\n", s5)
+	fmt.Printf("%s\n", bytes.Join(s6, []byte(", ")))
+}
+
+func use_math_pck() {
+	c := true
+	fmt.Println(c, math.Floor(1.7), math.Ceil(10.5))
 }
